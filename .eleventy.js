daef26b5c693f5421448d8c39a95158298707a0f
Tim VanReenen <tim.vanreenen@gmail.com>
2025-05-10 21:57:33 -0400

    Add favicons

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,11 @@ module.exports = function(eleventyConfig) {
     // allows assetes to be colocated with the posts
     eleventyConfig.addPassthroughCopy("posts/**/*.{jpg,jpeg,png,gif,svg,webp,pdf}");
     eleventyConfig.addPassthroughCopy("vanreenen-og.png");
+    // Copy favicon files
+    eleventyConfig.addPassthroughCopy("favicon.ico");
+    eleventyConfig.addPassthroughCopy("favicon-32x32.png");
+    eleventyConfig.addPassthroughCopy("favicon-16x16.png");
+    eleventyConfig.addPassthroughCopy("apple-touch-icon.png");
     // Copy CSS files
     eleventyConfig.addPassthroughCopy("css");
     // allows the post to be organized by folder
